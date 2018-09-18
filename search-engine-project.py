2c4ebdddacadc95a5377c8ae8220c47e8ec878b0
Emily M. Hoang <43097023+ehoang40@users.noreply.github.com>
2018-09-17 22:37:46 -0700

    cleaning-code-1

--- search-engine-project.py
+++ search-engine-project.py
@@ -1,190 +1,227 @@
 # Emily M. Hoang
-# spring quarter 2016
+# Spring quarter 2016 project
+# Edited Sept. 2018
+
 
 import glob
 
-# pre: needs list of punctuation symbols and list of stop words
-# post returns search phrase and list of punctuation
-def get_phrase(punctuation, stopWords):
-    #ONE BIG WHILE LOOP
-    validInput = False
-    while not(validInput): #while input is NOT valid
-        search_phrase = input("Enter the phrase to search: ")
-        search_phrase = search_phrase.split(" ") 
 
-        #Check for digits
-        digit = test_for_digits(search_phrase)
 
-        #Clean up  
-        search_phrase = remove_punctuation(search_phrase,punctuation)
-        search_phrase = remove_stopwords(search_phrase, stopWords)
+# pre: needs list of words
+# post: returns list with all words lowercase
+def all_lowercase(words_list):
+    for i in range(len(words_list)):
+        words_list[i] = words_list[i].lower()
+    return words_list
 
-        #CHECK LENGTH
-        less_than_two = True
-        length = 0
-        for element in search_phrase:
-            for char in element:
-                length += 1
-                
-        if length >= 2:
-            less_than_two = False
-
-        if digit == False and less_than_two == False: #if there are no digits and length
-                                                      #is not less than 2
-            validInput = True
-        else:
-            print("Invalid entry; try again.")
-
-    return search_phrase, punctuation
 
 
 # pre: needs search phrase
-# post: returns flag "digit".  "True" means search phrase contains
-# digits; "False means it does not contain digits.
-def test_for_digits(search_phrase):
-    digit = False #set flag, "False" means no digits
-    for element in search_phrase:
-        if element.isdigit():
-            digit = True
-    return digit
+# post: returns length of search phrase
+def check_length(s_phrase):
+    counter = 0
+    for element in s_phrase:
+        for char in element:
+            counter += 1
+
+    return counter
 
 
-# pre: needs list to clean and list of stop words
-# post: returns list w/o stop words
-def remove_stopwords(to_clean, stopWords):
-    clean = []
 
+# pre: none
+# post: returns list of all filenames in current directory
+# with .txt extension
+def getFilesInDir():
+    file_names = glob.glob('./*.txt') 
+    for i in range(len(file_names)):
+        file_names[i] = file_names[i][2:]
+
+    print("There are", len(file_names), ".txt files in this directory.")
+    print(file_names)
+    return file_names
+
+
+
+# pre: needs list of stop words and list of punctuation symbols
+# post returns valid search phrase
+def get_phrase(stop_l, punc_symbols):
+    invalid_input = True
+    while invalid_input: #while input is invalid
+        search = input("Enter the phrase to search: ")
+        search = search.split(" ") 
+
+        #Check for digits
+        contains_digit = test_for_digits(search)
+        if contains_digit == True:
+            print("Invalid entry; try again.")
+
+        else:
+            #Clean search_phrase  
+            search = remove_punctuation(search, punc_symbols)
+            search = remove_stopwords(search, stop_l)
+
+            #Check length
+            length = check_length(search)
+            if length >= 1:
+                invalid_input = False
+            else:
+                print("Invalid entry; try again.")
+
+    return search
+
+
+
+# pre: no argument
+# post: returns list of stop words
+def get_stop_words():
+    stop_list = [] #create list for stop words
+    stop_file = open("StopWords.csv", "r")
+    for l in stop_file:
+        l = l.rstrip()
+        stop_list.append(l)
+    stop_file.close()
+    return stop_list
+
+
+
+# pre: returns list of all filenames in current directory with .txt extension,
+# search phrase, list of punctuation, and list of stop words.
+# post: reads, processes, and closes each file.
+def process_all(all_files, search_p, punc_l, stop_l):
+    for i in range(len(all_files)): #open and process files one at a time
+        infile = all_files[i] 
+        current_file = open(infile, "r") 
+
+        process_file(infile, current_file, search_p, punc_l, stop_l)
+
+        current_file.close()
+
+
+        
+# pre: needs name of file to process, the actual file to process, search phrase,
+# list of puntuation, and list of stop words.
+# post: prints the number of times that search phrase appears in file
+def process_file(name_file, afile, search_phr, pun_list, st_list):
+    occurrences = 0
+    for l in afile:
+        l = l.split(" ") 
+        
+        #clean up each line, make lowercase
+        l = remove_punctuation(l, pun_list) 
+        l = remove_stopwords(l, st_list)
+        all_lowercase(l)
+
+##        index = -1
+##        for el in l:
+##            index += 1
+##            if index <= (len(search_phr)-1):
+##                if search_phr[index] == el:
+##                    if index == len(search_phr):
+##                        occurrences += 1
+                    
+        index = 0
+        for el in l:
+            if search_phr[index] == el:
+                index += 1
+            else:
+                index = 0
+            if index == len(search_phr):
+                occurrences += 1
+                index = 0
+
+    print("File", name_file, "has", occurrences, "occurrences of the phrase", search_phr)
+
+
+
+# pre: returns list of all filenames in current directory with .txt extension,
+# search phrase, list of punctuation, and list of stop words.
+# post: reads, processes, and closes each valid file input by user.
+def process_some(all_files, search_p, punc_l, stop_l):
+
+    while True: 
+        number_files = int(input("How many files do you want to search in? "))
+        if number_files < 0 or number_files > len(all_files):  # if number of files is less than 0
+                                                          # or more than in directory
+            print("Invalid entry; try again.")
+        else:
+            break
+    
+    for i in range(number_files):
+        invalid_input = True
+        while invalid_input:
+            file_name = input("Enter the file name with extension: ")
+            file_name_list = list(file_name) #convert file name into a list
+            if file_name_list[len(file_name_list)-1]=="t" and \
+               file_name_list[len(file_name_list)-2]=="x" and \
+               file_name_list[len(file_name_list)-3]=="t" and \
+               file_name_list[len(file_name_list)-4]=="." and \
+               file_name in all_files: # if file name has ".txt" extention AND exists in directory
+                break
+            else:
+                print("We can only search in existing .txt files.")
+
+        current_file = open(file_name, "r")
+        process_file(file_name, current_file, search_p, punc_l, stop_l)
+        current_file.close()   
+
+
+        
+# pre: needs phrase list to clean and list of punctuation symbols
+# post: returns phrase list without puncutation
+def remove_punctuation(phrase_list, punc_list):
+    index = -1
+    for word in phrase_list:
+        index += 1
+        for el in punc_list:
+            if el in word:
+                no_punc_word = word.rstrip(el) #strip ending punctuation symbol 
+                phrase_list[index] = no_punc_word #replace word into to_clean without punctuation
+
+    return phrase_list
+
+
+
+# pre: needs phrase list to clean and list of stop words
+# post: returns phrase list without stop words
+def remove_stopwords(to_clean, stop_w):
+    clean = [] #create list for phrase list without stop words
     for word in to_clean:
-        if word not in stopWords:
+        if word not in stop_w: 
             clean.append(word)
 
     return clean
 
 
-# pre: needs list to clean and list of punctuation symbols
-# post: returns list w/o puncutation
-def remove_punctuation(to_clean,punctuation):
-    search_phrase = [""]*len(to_clean) #create new list with same length as search_phrase (w/o stop
-                                     #words).  This will become new search phrase, but without
-                                     #punctuation.
-    index = -1
-    for word in to_clean:
-        index += 1
-        for el in punctuation:
-            if el in word:
-                word2 = word.rstrip(el) #strip ending punctuation symbol off word
-                search_phrase[index] = word2 #replace with cleaned up word
-                break
-            else:
-                search_phrase[index] = word
-    return search_phrase
 
-# pre: needs text file, search phrase, list of punctuation symbols, and list of
-# stop words
-# post: accepts files, processes them, and prints results of search query
-def filesToProcess(file,search_phrase, punctuation, stopWords):
-    print("There are", len(file), ".txt files in this directory.")
-    choice = input("Type Y to search all the txt files in the current" \
-                   " folder: ")
-
-    if choice == "Y": 
-        for i in range(len(file)): #open and process each file one at a time
-            infile = file[i] 
-            file_to_process = open(infile, "r") 
-
-            occurences = processFile(file_to_process, search_phrase, punctuation, stopWords)
-            print("File", infile, "has", occurences, "occurences of the phrase", search_phrase)
-            file_to_process.close()
-    else:
-        number_files = int(input("How many files do you want to search in? "))
-        while not(number_files > 0) or not(number_files <= len(file)): #while number of files
-        #entered is not greater than 0 AND not less than or equal to number of files in directory
-            print("Invalid entry; try again.")
-            number_files = int(input("How many files do you want to search in? "))
-
-        for i in range(number_files):
-            validInput = False
-            while not(validInput):
-                name_of_file = input("Enter the file name with extension: ")
-                name_of_file2 = list(name_of_file) #turn name of file entered into a list
-                if name_of_file2[len(name_of_file2)-1]=="t" and \
-                   name_of_file2[len(name_of_file2)-2]=="x" and \
-                   name_of_file2[len(name_of_file2)-3]=="t" and \
-                   name_of_file2[len(name_of_file2)-4]=="." and \
-                   name_of_file in file: #if last four characters of file name is ".txt" AND
-                   #the name exists in the directory
-                    validInput = True
-                else:
-                    print("We can only search in .txt files which already exist.")
-
-            file_to_process = open(name_of_file, "r")
-            occurences = processFile(file_to_process, search_phrase, punctuation, stopWords)
-            print("File", name_of_file, "has", occurences, "occurences of the phrase", search_phrase)
-            file_to_process.close()   
+# pre: needs search phrase 
+# post: returns flag "digit".  "True" means search phrase contains
+# digits; "False" means it does not contain digits.
+def test_for_digits(phrase):
+    digit = False #set flag where "False" means no digits
+    for el in phrase:
+        if el.isdigit():
+            digit = True
+            break
+    return digit
 
 
-# pre: needs file to process, search file, list of punctuation symbols, and list of
-# stop words
-# post: returns number of times search phrase appears in file
-def processFile(file, search_phrase, punctuation, stopWords):
-    occurences = 0
-    for l in file:
-        l = l.split(" ") 
-        
-        #clean up each line, make lowercase
-        l = remove_punctuation(l, punctuation) 
-        l = remove_stopwords(l, stopWords)
-        l = all_lowercase(l)
 
-        index = 0
-        for element in l:
-            if search_phrase[index] == element:
-                index += 1
-            else:
-                index = 0
-            if index == len(search_phrase):
-                occurences += 1
-                index = 0
-    return occurences
-
-# pre: no argument
-# post: returns list of stop words
-def get_stopWords():
-    stopWords = [] #list for stop words
-    file = open("StopWords.csv", "r")
-    for l in file:
-        l = l.rstrip()
-        stopWords.append(l)
-    file.close()
-    return stopWords
-
-       
-### pre: none
-### post: returns list of all filenames in current directory
-##  with .txt extension
-def getFilesInDir():
-    filenames = glob.glob('./*.txt') 
-    for i in range(len(filenames)):
-        filenames[i] = filenames[i][2:]
-    return filenames
-
-# pre: needs list with only letters
-# post: returns list with all lower-case letters
-def all_lowercase(list):
-    for i in range(len(list)):
-        list[i] = list[i].lower()
-    return list
-    
 def main():
     punctuation = [".", ",",":",";","!","?"]
-    stopWords = get_stopWords()
-    stopWords2 = all_lowercase(stopWords)
-    choice = "Y"
-    while choice == "Y":
-        search_phrase, punctuation = get_phrase(punctuation, stopWords)
-        search_phrase2 = all_lowercase(search_phrase)
-        filenames = getFilesInDir()
-        filesToProcess(filenames, search_phrase, punctuation, stopWords)
-        choice = input("Do you want to search again?  Say Y to continue searching: ")
+    stop_words = get_stop_words() 
+    all_lowercase(stop_words)
+    choice = "Yes"
+    while choice == "Yes":
+        search_phrase = get_phrase(stop_words, punctuation)
+        all_lowercase(search_phrase)
+        files = getFilesInDir()
+        choice = input("Type Y to search all the txt files in the current" \
+                       " folder: ")
+        if choice == "Y":
+            process_all(files, search_phrase, punctuation, stop_words)
+        else:
+            process_some(files, search_phrase, punctuation, stop_words)
+
+        choice = input("Do you want to search again?  Type \"Yes\" to continue searching: ")
 
 main()
